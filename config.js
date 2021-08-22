1bd8b1f6a4fafbd60a750556a27f69c433fcb615
trang10 <89339443+trang10@users.noreply.github.com>
2021-08-22 22:01:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Linggg à!',
-    introDesc: `Trái đất vốn lạ thường mà sao bạn cứ đi nhầm đường
-    lạc vào tim tớ lẻ loi đằng sau chữ yêu đây là Thương,
+    introDesc: `Trái đất vốn lạ thường mà sao m cứ đi nhầm đường
+    lạc vào tim t lẻ loi đằng sau chữ yêu đây là Thương,
     btnIntro: 'hihi',
-    title: 'Phải chăng c đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng c đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng m đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng m đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là iu Trang <33',
     btnNo: 'Không, bạn à :3',
     question:'Trên thế giới này có hơn 7 tỷ người mà sao cậu lại thích tớ <33',
     btnReply: 'Gửi cho Trang <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'tớ biết mà 🥰. Yêu cậu  nhiều nhiều 😘😘',
+    mess: 't biết mà 🥰. Yêu m nhiều nhiều 😘😘',
     messDesc: 'Hôm nào hết dịch gặp nhau nhóoo.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100064754330861
